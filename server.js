c7b8708e00adcbd57a68360b6c181b981b5f3514
TeoWebProjects <teowebdevprojects@gmail.com>
2023-05-21 18:33:43 +0300

    add body limit 2

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 
-const app = express()
+const app = express({ limit: '100mb' })
 app.use(express.json())
 app.use(cors())
 
 app.use(express.static('./public'))
 app.use(express.bodyParser({ limit: '100mb' }))
+app.use(express.urlencoded({ extended: true, limit: '100mb' }))
 
 app.get('/', function (req, res) {
   res.send('Test Server!')
